32c9fbbc28f26dc3a2060225666c5b234c99bf98
mariyaeggs <mariyawilson@live.com>
2017-07-25 04:47:14 -0700

    Initialize contacts.

--- reactnd-contacts-complete/src/App.js
+++ reactnd-contacts-complete/src/App.js
@@ -1,54 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
-import * as ContactsAPI from './utils/ContactsAPI'
-
-class App extends Component {
-  state = {
-    contacts: []
+const contacts = [
+  {
+    "id": "mariya",
+    "name": "Mariya Eggensperger",
+    "email": "mariya@reactpractice.com",
+    "avatarURL": "http://localhost:5001/mariya.jpg"
+  },
+  {
+    "id": "scott",
+    "name": "Scott Eggensperger",
+    "email": "scott@reactpractice.com",
+    "avatarURL": "http://localhost:5001/scott.jpg"
+  },
+  {
+    "id": "sofia",
+    "name": "Sofia Eggensperger",
+    "email": "sofia@reactpractice.com",
+    "avatarURL": "http://localhost:5001/sofia.jpg"
+  }, 
+  {
+    "id": "irina",
+    "name": "Irina Wilson",
+    "email": "irina@reactpractice.com",
+    "avatarURL": "http://localhost:5001/irina.jpg"
   }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
-      </div>
-    )
-  }
-}
-
-export default App;
+]
